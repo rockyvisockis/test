90181a6064da93d02e808184e8ea5ce2f02c40e1
rockyvisockis <rokas.visockis@sky.uk>
2022-04-07 14:53:11 +0100

    testing fly command

--- scripts/helloworld.sh
+++ scripts/helloworld.sh
@@ -5,8 +5,7 @@ DEBIAN_FRONTEND=noninteractive apt-get install apt-utils -y > /dev/null
 DEBIAN_FRONTEND=noninteractive apt-get install -y curl > /dev/null
 apt-get -qq -y install jq > /dev/null
 
-# Use API? 
-
+# Use API?
 export CONCOURSEURL="https://concourse.at.sky"
 
 export DOWLOADLINK="$CONCOURSEURL/api/v1/cli"
@@ -16,4 +15,6 @@ curl $DOWLOADLINK -G -d 'arch=amd64' -d 'platform=linux' -o 'fly'
 mv fly /usr/local/bin
 chmod 0755 /usr/local/bin/fly
 
-cat $CONCOURSEURL/api/v1/workers
+/usr/local/bin/fly --target="main" login \
+    --concourse-url $CONCOURSEURL \
+    --username "concourse" --password "$concourse_user_secret" > "fly_login.txt"
