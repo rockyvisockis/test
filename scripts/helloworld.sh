cea196265aa65932b053b2cc731d3a54854e14cc
rockyvisockis <rokas.visockis@sky.uk>
2022-04-07 10:50:54 +0100

    added shell script

--- scripts/helloworld.sh
+++ scripts/helloworld.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-echo hello-world
+apt-get update > /dev/null
+DEBIAN_FRONTEND=noninteractive apt-get install dialog apt-utils -y > /dev/null
+DEBIAN_FRONTEND=noninteractive apt-get install -y curl > /dev/null
+apt-get -qq -y install jq > /dev/null
+
+
+export CONCOURSEURL="https://concourse.at.sky"
+
+export DOWLOADLINK="$CONCOURSEURL/api/v1/cli"
+
+curl $DOWLOADLINK -G -d 'arch=amd64' -d 'platform=linux' -o 'fly'
+
+mv fly /usr/local/bin
+chmod 0755 /usr/local/bin/fly
+
+/usr/local/bin/fly --target="main" login \
+    --concourse-url $CONCOURSEURL \
+    --username "concourse" --password "$concourse_user_secret" > "fly_login.txt"
