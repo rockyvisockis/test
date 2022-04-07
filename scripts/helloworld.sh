e16107688fcf0e6ebab4a178d489ba4a906ba6fd
rockyvisockis <rokas.visockis@sky.uk>
2022-04-07 12:11:02 +0100

    testing fly command

--- scripts/helloworld.sh
+++ scripts/helloworld.sh
@@ -5,6 +5,7 @@ DEBIAN_FRONTEND=noninteractive apt-get install apt-utils -y > /dev/null
 DEBIAN_FRONTEND=noninteractive apt-get install -y curl > /dev/null
 apt-get -qq -y install jq > /dev/null
 
+# Use API? 
 
 export CONCOURSEURL="https://concourse.at.sky"
 
@@ -15,4 +16,4 @@ curl $DOWLOADLINK -G -d 'arch=amd64' -d 'platform=linux' -o 'fly'
 mv fly /usr/local/bin
 chmod 0755 /usr/local/bin/fly
 
-
+fly --version
