ba8d900b80d4e9de443cfe02e4be1c35d6990908
rockyvisockis <rokas.visockis@sky.uk>
2022-04-08 17:04:18 +0100

    removed login block

--- scripts/helloworld.sh
+++ scripts/helloworld.sh
@@ -5,24 +5,19 @@ DEBIAN_FRONTEND=noninteractive apt-get install apt-utils -y > /dev/null
 DEBIAN_FRONTEND=noninteractive apt-get install -y curl > /dev/null
 apt-get -qq -y install jq > /dev/null
 
-# Vars
-CONCOURSE_URL="https://concourse.at.sky"
-CONCOURSE_USER="concourse"
-CONCOURSE_PASS="$concourse_user_secret"
-CONCOURSE_TARGET="cec-training"
-FLY_ENDPOINT="/api/v1/cli?arch=amd64&platform=linux"
-AUTH_ENDPOINT="/sky/login"
+# Use API?
+export CONCOURSEURL="https://concourse.at.sky"
 
-# Get fly binary
-TMP_DIR=$(mktemp -d) && trap "rm -rf ${TMP_DIR}" EXIT
-FLY_BIN="${TMP_DIR}/fly"
-curl -o "${FLY_BIN}" "${CONCOURSE_URL}/api/v1/cli?arch=amd64&platform=linux"
-chmod a+x "${FLY_BIN}"
+export DOWLOADLINK="$CONCOURSEURL/api/v1/cli"
 
-COOKIE_FILE="${TMP_DIR}/cookie.txt"
-AUTH2="$(curl -b ${COOKIE_FILE} -c ${COOKIE_FILE} -s -o /dev/null -L "${CONCOURSE_URL}${AUTH_ENDPOINT}" -D - | \
-    grep "Location: /sky/issuer/auth" | cut -d ' ' -f 2 | tr -d '\r')"
-curl -o /dev/null -s -b ${COOKIE_FILE} -c ${COOKIE_FILE} -L --data-urlencode "login=${CONCOURSE_USER}" \
-    --data-urlencode "password=${CONCOURSE_PASS}" "${CONCOURSE_URL}${AUTH2}"
-OAUTH_TOKEN=$(cat ${COOKIE_FILE} | grep -o 'Bearer .*$' | tr -d '"')
-echo $OAUTH_TOKEN | $FLY_BIN -t $CONCOURSE_TARGET login -c $CONCOURSE_URL -n main
+curl $DOWLOADLINK -G -d 'arch=amd64' -d 'platform=linux' -o 'fly'
+
+mv fly /usr/local/bin
+chmod 0755 /usr/local/bin/fly
+
+
+fly -t cec-training login -c $CONCOURSEURL \
+    --username "concourse" \
+    --password "$concourse_user_secet" 
+
+echo bearer Sbnv1iEk16R8mAu542xrfSHPrOjpqFFiAAAAAA
