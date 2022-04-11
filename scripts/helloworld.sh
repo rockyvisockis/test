8066fcc6636e19e714ef925b85f51d0d68396163
rockyvisockis <rokas.visockis@sky.uk>
2022-04-11 11:18:29 +0100

    updated login

--- scripts/helloworld.sh
+++ scripts/helloworld.sh
@@ -15,7 +15,7 @@ curl $DOWLOADLINK -G -d 'arch=amd64' -d 'platform=linux' -o 'fly'
 mv fly /usr/local/bin
 chmod 0755 /usr/local/bin/fly
 
-fly -t cec-training login -c $CONCOURSEURL \
+fly -t main login -c $CONCOURSEURL \
     --username "concourse" \
     --password "$concourse_user_secet" 
 
