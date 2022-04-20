157f0141dba681be487d449f46225158c1096d5c
rockyvisockis <rokas.visockis@sky.uk>
2022-04-20 16:29:44 +0100

    made changes to notify

--- scripts/helloworld.sh
+++ scripts/helloworld.sh
@@ -4,7 +4,7 @@ apt-get -qq update
 DEBIAN_FRONTEND=noninteractive apt-get -qq install apt-utils -y > /dev/null 2>&1
 DEBIAN_FRONTEND=noninteractive apt-get -qq install -y curl > /dev/null 2>&1
 apt-get -qq install jq -y > /dev/null 2>&1
-apt-get  install awscli -y > /dev/null 
+
 
 
 export DOWNLOADLINK="$concourse_url/api/v1/cli"
