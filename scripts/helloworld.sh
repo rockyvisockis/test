55e8ec9512e252070d81a212624c837dc22e40da
rockyvisockis <rokas.visockis@sky.uk>
2022-04-20 11:06:23 +0100

    changed notification settings

--- scripts/helloworld.sh
+++ scripts/helloworld.sh
@@ -5,17 +5,15 @@ DEBIAN_FRONTEND=noninteractive apt-get install apt-utils -y > /dev/null
 DEBIAN_FRONTEND=noninteractive apt-get install -y curl > /dev/null
 apt-get -qq -y install jq > /dev/null
 
-# Use API for concourse URL?
-# Remove from aws?
-export CONCOURSEURL="https://concourse.at.sky"
-export DOWNLOADLINK="$CONCOURSEURL/api/v1/cli"
+
+export DOWNLOADLINK="$concourseurl/api/v1/cli"
 
 curl $DOWNLOADLINK -G -d 'arch=amd64' -d 'platform=linux' -o 'fly'
 
 mv fly /usr/local/bin
 chmod 0755 /usr/local/bin/fly
 
-/usr/local/bin/fly -t main login --team-name cec -c $CONCOURSEURL \
+/usr/local/bin/fly -t main login --team-name cec -c $concourseurl \
     --username "concourse" \
     --password "$concourse_user_secret" 
 
@@ -26,13 +24,30 @@ jq '.[] | select(.state == "retiring") | .name' workers.json > retiring.txt
 
 
 if [ -s retiring.txt ]; then
-    echo "Workers stuck in retiring"
+    echo "Workers stuck in 'retiring'"
     for worker in $(cat retiring.txt) ; do
-        /usr/local/bin/fly -t main prune-worker --worker $worker >> prune-info/file.txt
+        /usr/local/bin/fly -t main prune-worker --worker $worker >> file.txt
+        
     done
-    exit 1
+    
+    for outcome in $(cat file.txt)
+    do
+
+        case "$outcome" in
+            pruned)
+                echo "Workers have been pruned successfully" >> prune-info/pruned.txt   
+                exit 0
+                ;;
+            
+            *)
+                echo "ERRROR: Some error occurred while prunning workers" >> prune-info/pruned.txt
+                exit 1
+                ;;
+        esac
+    done 
+
 else
-    echo "All workers are running" 
+    echo "All workers are running" > prune-info/pruned.txt
     exit 0
 fi
     
