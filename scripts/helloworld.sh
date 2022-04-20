ab35db5c2f19c3d6bf556c3a65b079c212465dd1
rockyvisockis <rokas.visockis@sky.uk>
2022-04-20 16:01:50 +0100

    made changes to notify

--- scripts/helloworld.sh
+++ scripts/helloworld.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-apt-get -qq update 
-DEBIAN_FRONTEND=noninteractive apt-get -qq install apt-utils -y > /dev/null 2>&1
-DEBIAN_FRONTEND=noninteractive apt-get -qq install -y curl > /dev/null 2>&1
-apt-get -qq install jq -y > /dev/null 2>&1
+yum -qq update 
+DEBIAN_FRONTEND=noninteractive yum -qq install apt-utils -y > /dev/null 2>&1
+DEBIAN_FRONTEND=noninteractive yum -qq install -y curl > /dev/null 2>&1
+yum -qq install jq -y > /dev/null 2>&1
 
 
 export DOWNLOADLINK="$concourse_url/api/v1/cli"
