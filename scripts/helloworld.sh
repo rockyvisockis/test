6935373b5c8aaa0266ae56a52d227b6c55c995c4
rockyvisockis <rokas.visockis@sky.uk>
2022-04-25 10:45:41 +0100

    changed dependencies

--- scripts/helloworld.sh
+++ scripts/helloworld.sh
@@ -4,7 +4,7 @@
 apt-get -qq update 
 DEBIAN_FRONTEND=noninteractive apt-get -qq install apt-utils -y > /dev/null 2>&1
 DEBIAN_FRONTEND=noninteractive apt-get -qq install curl -y > /dev/null 2>&1
-DEBIAN_FRONTEND=noninteractive apt-get install awscli -d -y
+DEBIAN_FRONTEND=noninteractive apt-get install -qq awscli -y
 apt-get -qq install jq -y > /dev/null 2>&1
 
 
